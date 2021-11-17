158b96173c99652c6bfa31ed7280d16983f1d89c
Sergio Rodriguez <srodriguez@add123.com>
2021-11-16 23:05:51 -0500

    Added Lazy Loading for all Dicier Web Fonts

--- src/components/DicierIcon/index.js
+++ src/components/DicierIcon/index.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import loadFont from '../../fonts/loader'
+import * as modes from '../../constants/modes'
+import * as weights from '../../constants/weights'
 import './index.scss'
 
-const DicierIcon = ({ children, ...params }) =>
-  <span className='dicier' {...params}>
-    {children}
-  </span>
+const DicierIcon = ({ children, className, mode = modes.BLOCK, weight = weights.DARK, ...params }) => {
+  mode = mode?.toLowerCase()
+  weight = weight?.toLowerCase()
+  loadFont(mode, weight)
+
+  return (
+    <span {...params} className={buildClassName({ mode, weight, className })} >
+      {children}
+    </span>
+
+  )
+}
+
+const buildClassName = ({ mode, weight, className }) => {
+  const classes = ['dicier']
+  if (mode === modes.PIXEL) {
+    classes.push(['dicier-pixel'])
+  } else {
+    classes.push(['dicier', mode, weight].join('-'))
+  }
+  if (className) {
+    classes.push(className)
+  }
+  return classes.join(' ')
+}
 
 export default DicierIcon
---
+++ src/constants/modes.js
@@ -0,0 +1,4 @@
+export const PIXEL = 'pixel'
+export const ROUND = 'round'
+export const BLOCK = 'block'
+export const FLAT = 'flat'
